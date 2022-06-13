a55323aa7928365ea36c947f100c2e004b4d7f02
3d24rd0 <3d24rd0@gmail.com>
2022-06-13 22:46:36 +0200

    Update 02-flutter.sh

--- restoration_scripts/02-flutter.sh
+++ restoration_scripts/02-flutter.sh
@@ -2,7 +2,9 @@ git -C $HOME/Library/ clone https://github.com/flutter/flutter.git -b stable
 
 flutter precache
 
+sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+
 xcodebuild -runFirstLaunch
 
-
+# open android Studio install flutter and plugins
 flutter doctor --android-licenses
