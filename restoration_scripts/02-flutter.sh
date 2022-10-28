164d674e0e5a886e03f6573668076bd3a221c418
Eduardo Martínez Catalá <3d24rd0@gmail.com>
2022-10-28 07:41:17 +0200

    improve restoration

--- restoration_scripts/02-flutter.sh
+++ restoration_scripts/02-flutter.sh
@@ -4,9 +4,6 @@ flutter channel stable
 
 flutter precache
 
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-
-xcodebuild -runFirstLaunch
 
 # open android Studio install flutter and plugins
 flutter doctor --android-licenses
--- restoration_scripts/03-cocoapods.sh
+++ restoration_scripts/03-cocoapods.sh
@@ -1,4 +1,18 @@
-gem install cocoapods
+#!/usr/bin/env bash
 
-# silicon
-# $ sudo gem uninstall ffi && sudo gem install ffi -- --enable-libffi-alloc
+
+
+source "$DOTLY_PATH/scripts/core/_main.sh"
+
+
+if platform::is_macos; then
+  if platform::is_macos_arm; then
+     sudo gem uninstall ffi && sudo gem install ffi -- --enable-libffi-alloc
+     gem install cocoapods
+  else
+     gem install cocoapods
+  fi
+fi
+
+log::success "Done!"
+
